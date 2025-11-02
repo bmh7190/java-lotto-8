e22f7f40bc978383f1391e242657c0027f8c14f5
bmh7190 <bmh7190@naver.com>
2025-11-02 21:45:22 +0900

    feat(service): 로또 구매 개수 계산 기능 추가
    - calculateCount() 메서드를 통해 구입 금액을 기준으로 구매 가능한 로또 개수 계산
    - PRICE_PER_LOTTO 상수를 이용해 1장당 금액 단위로 나눈 정수값 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,10 +5,15 @@ import lotto.domain.WinningLotto;
 
 public class LottoService {
 
+    private static final int PRICE_PER_LOTTO = 1000;
+
     public WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber){
         return new WinningLotto(winningNumbers, bonusNumber);
     }
 
+    public int calculateCount(int purchaseAmount) {
+        return purchaseAmount / PRICE_PER_LOTTO;
+    }
 
 
 }
