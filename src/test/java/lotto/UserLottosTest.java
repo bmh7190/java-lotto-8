32132e2343d3cfa269ef06ce5e21ea4c13f2f98c
bmh7190 <bmh7190@naver.com>
2025-11-03 00:42:20 +0900

    fix: import 정리 및 패키지 경로 수정

--- src/test/java/lotto/UserLottosTest.java
+++ src/test/java/lotto/UserLottosTest.java
@@ -1,9 +1,10 @@
-package lotto.domain;
+package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.UserLottos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
