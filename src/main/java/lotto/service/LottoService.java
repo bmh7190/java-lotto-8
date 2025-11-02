858be677ca771fab3c0a56da58d770aa1e0d05e7
bmh7190 <bmh7190@naver.com>
2025-11-02 22:01:02 +0900

    feat(service): UserLottos 생성 기능 구현
    - LottoService가 LottoNumberGenerator 인터페이스를 주입받도록 수정
    - 번호 생성 전략을 랜덤/고정 구현체로 분리 (DI 구조 적용)
    - 구입 금액 기반으로 UserLottos 생성 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,14 +1,35 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.UserLottos;
 import lotto.domain.WinningLotto;
+import lotto.generator.LottoNumberGenerator;
 
 public class LottoService {
 
     private static final int PRICE_PER_LOTTO = 1000;
+    private final LottoNumberGenerator generator;
 
-    public WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber){
+    public LottoService(LottoNumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public WinningLotto createWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
         return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    public UserLottos createUserLottos(int purchaseAmount) {
+
+        int count = purchaseAmount / PRICE_PER_LOTTO;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(generator.generateNumbers()));
+        }
+
+        return new UserLottos(lottos);
     }
 
     public int calculateCount(int purchaseAmount) {
