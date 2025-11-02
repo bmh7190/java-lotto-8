2f3348da00ba03916d587ab7c324a5d3fd1bbb88
bmh7190 <bmh7190@naver.com>
2025-11-02 23:17:49 +0900

    feat(app): Application 엔트리포인트 추가 및 전체 실행 흐름 연결
    - main() 메서드에서 로또 프로그램 전체 실행 진입점 구성
    - 의존성 주입 방식으로 객체 생성하여 테스트 및 확장 용이성 확보

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.generator.RandomLottoNumberGenerator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        LottoController controller = new LottoController(
+                new InputView(),
+                new OutputView(),
+                new LottoService(new RandomLottoNumberGenerator())
+        );
+
+        controller.run();
     }
 }
