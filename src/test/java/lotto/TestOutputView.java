74e8deac698a4677cfda570feb8dbd29271ca21d
bmh7190 <bmh7190@naver.com>
2025-11-03 02:22:35 +0900

    test(view): LottoController 출력 검증용 TestOutputView 추가
    - OutputView를 상속한 TestOutputView 생성
    - 출력된 로또 목록, 에러 메시지, 당첨 통계, 수익률을 저장하도록 구현
    - LottoController 테스트에서 콘솔 출력 대신 상태 기반 검증 가능

---
+++ src/test/java/lotto/TestOutputView.java
@@ -0,0 +1,33 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestOutputView extends OutputView {
+
+    public final List<String> errors = new ArrayList<>();
+    public final List<List<Integer>> printedLottos = new ArrayList<>();
+    public List<Integer> printedWinningCounts;
+    public Double printedProfitRate;
+
+    @Override
+    public void printPurchasedLottos(List<List<Integer>> lottos) {
+        printedLottos.addAll(lottos);
+    }
+
+    @Override
+    public void printWinningStatistics(List<Integer> counts) {
+        this.printedWinningCounts = counts;
+    }
+
+    @Override
+    public void printProfitRate(double profitRate) {
+        this.printedProfitRate = profitRate;
+    }
+
+    @Override
+    public void printError(String message) {
+        errors.add(message);
+    }
+}
+
